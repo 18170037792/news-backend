f3b7f94ef48d27b4b467c5b77e5c676b19b76659
18170037792 <1071367833@qq.com>
2018-12-09 22:21:37 +0800

    整合模板

--- news-backend/src/main/java/com/xiafei/newsbackend/controller/HelloController.java
+++ news-backend/src/main/java/com/xiafei/newsbackend/controller/HelloController.java
@@ -1,14 +1,30 @@
 package com.xiafei.newsbackend.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping("/hello")
-    public String hello(){
-        return "templates/home/about";
+    @RequestMapping("/contact")
+    public String contact(){
+        return "home/contact";
+    }
+
+    @RequestMapping("/about")
+    public String about(){
+        return "home/about";
+    }
+
+    @RequestMapping("/index")
+    public String index(){
+        return "home/index";
+    }
+
+    @RequestMapping("/single")
+    public String single(){
+        return "home/single";
     }
 }
